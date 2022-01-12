33827c8e702256866d0f74a63f23982befe2b174
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-01-12 20:31:03 +0000

    added patch and delete for items

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -15,6 +15,23 @@ export const getInventory = async (req, res) => {
     res.status(400).json({ message: "Could not fetch inventory" });
   }
 };
+// get a specific user item
+export const getItem = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.user.email });
+    if (!user) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    const { id } = req.params;
+    const item = await Item.findOne({ id, user_id: user.id });
+    if (!item) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json({ item });
+  } catch (error) {
+    res.status(400).json({ message: "Could not fetch inventory" });
+  }
+};
 // create/add an item
 export const addItem = async (req, res) => {
   try {
@@ -40,5 +57,59 @@ export const addItem = async (req, res) => {
       console.error("VALIDATION ERRORS:", Object.keys(error.errors));
     }
     res.status(400).json({ message: "Could not add item" });
+  }
+};
+// patch/update an item
+export const editItem = async (req, res) => {
+  try {
+    //   get authenticated user from request
+    const authUser = req.user;
+    const user = await User.findOne({ email: authUser.email });
+    if (!user) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    const { name, description } = req.body;
+    const { id } = req.params;
+    // find object with given id
+    const item = await Item.findById(id);
+    if (!item) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    // patch item data
+    item.name = name || item.name;
+    item.description = description || item.description;
+
+    // validate schema and save
+    await item.validate();
+    await item.save();
+
+    res.status(200).json({ message: "Item updated", item });
+  } catch (error) {
+    if (error instanceof DB_VALIDATION_ERROR) {
+      console.error("VALIDATION ERRORS:", Object.keys(error.errors));
+    }
+    res.status(400).json({ message: "Could not add item" });
+  }
+};
+
+// delete a specific user item
+export const deleteItem = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.user.email });
+    if (!user) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    const { id } = req.params;
+    const item = await Item.findOne({ id, user_id: user.id });
+    if (!item) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    const deleted = await Item.deleteOne({ id });
+    if (!deleted.deletedCount) {
+      res.status(400).json({ message: "Could not delete item" });
+    }
+    res.status(200).json({ message: "Item deleted" });
+  } catch (error) {
+    res.status(400).json({ message: "Could not fetch inventory" });
   }
 };
